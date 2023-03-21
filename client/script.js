f16ca259dcb7898e864550f45c24f6dfd8e2b344
Gamma-Burst <christophe.v.mercier@gmail.com>
2023-03-21 18:27:27 +0100

    update the server url

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://codex-ai-o1tr.onrender.com', {
+    const response = await fetch('https://codex-1z8x.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
