a6f800515c232287d28dbc6d544421c0fbbafd25
Gamma-Burst <christophe.v.mercier@gmail.com>
2023-03-22 21:45:26 +0100

    update the server url

--- client/script.js
+++ client/script.js
@@ -67,80 +67,90 @@ function storeConversation() {
   localStorage.setItem('conversation', conversation)
 }
 
-const handleSubmit = async (e) => {
-  e.preventDefault()
+const jokes = [
+  'Why was the math book sad? Because it had too many problems.',
+  'Why did the tomato turn red? Because it saw the salad dressing.',
+  'What did the janitor say when he jumped out of the closet? "Supplies!"',
+  'How do you make a tissue dance? You put a little boogey in it!',
+  'Why did the scarecrow win an award? Because he was outstanding in his field.',
+  'Why did the hipster burn his tongue? He drank his coffee before it was cool.',
+  'Why don’t scientists trust atoms? Because they make up everything.',
+  'Why don’t oysters give to charity? They’re shellfish.',
+  'Why did the chicken cross the playground? To get to the other slide.',
+  'Why did the stadium get hot after the game? Because all of the fans left.',
+];
 
-  const data = new FormData(form)
+function getJoke() {
+  const randomIndex = Math.floor(Math.random() * jokes.length)
+  return jokes[randomIndex]
+}
 
-  // user's chatstripe
-  chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
-
-  // to clear the textarea input
-  form.reset()
-
-  // bot's chatstripe
-  const uniqueId = generateUniqueId()
-  chatContainer.innerHTML += chatStripe(true, ' ', uniqueId)
-
-  // to focus scroll to the bottom
-  chatContainer.scrollTop = chatContainer.scrollHeight
-
-  // specific message div
-  const messageDiv = document.getElementById(uniqueId)
-
-  // messageDiv.innerHTML = "..."
-  loader(messageDiv)
-
+async function getBotResponse(prompt) {
   const response = await fetch('https://codex-1z8x.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      prompt: data.get('prompt'),
+      prompt: prompt,
     }),
   })
-
-  clearInterval(loadInterval)
-  messageDiv.innerHTML = ' '
 
   if (response.ok) {
     const data = await response.json()
     const parsedData = data.bot.trim() // trims any trailing spaces/'\n'
-
-    typeText(messageDiv, parsedData)
-
-    storeConversation() // Store the conversation in local storage after each message is sent/received
+    return parsedData
   } else {
     const err = await response.text()
-
-    messageDiv.innerHTML = 'Something went wrong'
-    alert(err)
+    throw new Error(err)
   }
 }
 
-form.addEventListener('submit', handleSubmit)
-form.addEventListener('keyup', (e) => {
-  if (e.keyCode === 13) {
-    handleSubmit(e)
+async function handleUserInput(input) {
+  chatContainer.innerHTML += chatStripe(false, input)
+  form.reset()
+  const uniqueId = generateUniqueId();
+chatContainer.innerHTML += chatStripe(true, ' ', uniqueId);
+
+// to focus scroll to the bottom
+chatContainer.scrollTop = chatContainer.scrollHeight;
+
+// specific message div
+const messageDiv = document.getElementById(uniqueId);
+
+// messageDiv.innerHTML = "..."
+loader(messageDiv);
+
+const response = await fetch('https://codex-1z8x.onrender.com', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    prompt: data.get('prompt'),
+  }),
+});
+
+clearInterval(loadInterval);
+messageDiv.innerHTML = ' ';
+
+if (response.ok) {
+  const data = await response.json();
+  const parsedData = data.bot.trim(); // trims any trailing spaces/'\n'
+
+  // If the prompt contains the word "joke", randomly choose from an array of jokes
+  if (data.prompt.toLowerCase().includes('joke')) {
+    const jokes = ['Why was the math book sad? Because it had too many problems.', 'I told my wife she was drawing her eyebrows too high. She looked surprised.', 'Why do we tell actors to “break a leg?” Because every play has a cast.']
+    const joke = jokes[Math.floor(Math.random() * jokes.length)];
+    typeText(messageDiv, joke);
+  } else {
+    typeText(messageDiv, parsedData);
   }
-})
-// Restore the conversation from local storage on page load
-window.addEventListener('load', function() {
-  var conversation = JSON.parse(localStorage.getItem('conversation'));
-  if (conversation && conversation.length > 0) {
-  conversation.forEach(function(message) {
-  addMessageToChat(message);
-  });
-  }
-  });
-  
-  // Save the conversation to local storage every time a message is sent
-  function saveConversationToLocalStorage() {
-  localStorage.setItem('conversation', JSON.stringify(messages));
-  }
-  
-  // Clear the conversation from local storage
-  function clearConversationFromLocalStorage() {
-  localStorage.removeItem('conversation');
-  }
+
+  storeConversation(); // Store the conversation in local storage after each message is sent/received
+} else {
+  const err = await response.text();
+
+  messageDiv.innerHTML = 'Something went wrong';
+  alert(err);
+}}
