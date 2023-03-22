61a70c830198b338c2a4df9dfbc3fcaf6726a13d
Gamma-Burst <christophe.v.mercier@gmail.com>
2023-03-22 21:30:08 +0100

    update the server url

--- client/script.js
+++ client/script.js
@@ -125,82 +125,22 @@ form.addEventListener('keyup', (e) => {
     handleSubmit(e)
   }
 })
-
 // Restore the conversation from local storage on page load
-window.addEventListener('load', () => {
-  const conversation = JSON.parse(localStorage.getItem('conversation')) || [];
-  conversation.forEach(({ isUser, message }) => {
-    if (isUser) {
-      addMessageToChat(message, 'user');
-    } else {
-      addMessageToChat(message, 'bot');
-    }
+window.addEventListener('load', function() {
+  var conversation = JSON.parse(localStorage.getItem('conversation'));
+  if (conversation && conversation.length > 0) {
+  conversation.forEach(function(message) {
+  addMessageToChat(message);
   });
-});
-
-// Save the conversation to local storage
-function saveConversationToLocalStorage() {
-  const conversation = getConversationFromChat();
-  localStorage.setItem('conversation', JSON.stringify(conversation));
-}
-
-// Send a message to the bot
-function sendMessageToBot(message) {
-  // Send the message to the server using fetch or XHR
-  // ...
-  // When the response comes back, add the bot's response to the chat
-  const botResponse = 'This is a dummy response from the bot';
-  addMessageToChat(botResponse, 'bot');
-  // Save the conversation to local storage
-  saveConversationToLocalStorage();
-}
-
-// Add a message to the chat
-function addMessageToChat(message, sender) {
-  // Create a new message element
-  const messageElement = document.createElement('div');
-  messageElement.classList.add('message');
-  messageElement.classList.add(sender);
-
-  // Set the message text
-  const messageText = document.createElement('div');
-  messageText.classList.add('message-text');
-  messageText.textContent = message;
-  messageElement.appendChild(messageText);
-
-  // Add the message element to the chat
-  const chat = document.querySelector('#chat');
-  chat.appendChild(messageElement);
-}
-
-// Get the conversation from the chat
-function getConversationFromChat() {
-  const conversation = [];
-  const messages = document.querySelectorAll('.message');
-  messages.forEach((message) => {
-    const isUser = message.classList.contains('user');
-    const messageText = message.querySelector('.message-text').textContent;
-    conversation.push({ isUser, message: messageText });
+  }
   });
-  return conversation;
-}
-
-// Attach a submit event listener to the form
-form = document.querySelector('form');
-form.addEventListener('submit', (event) => {
-  // Prevent the default form submission behavior
-  event.preventDefault();
-
-  // Get the user's message from the input field
-  const input = document.querySelector('#input');
-  const message = input.value.trim();
-
-  // Add the user's message to the chat
-  addMessageToChat(message, 'user');
-
-  // Send the message to the bot
-  sendMessageToBot(message);
-
-  // Clear the input field
-  input.value = '';
-});
+  
+  // Save the conversation to local storage every time a message is sent
+  function saveConversationToLocalStorage() {
+  localStorage.setItem('conversation', JSON.stringify(messages));
+  }
+  
+  // Clear the conversation from local storage
+  function clearConversationFromLocalStorage() {
+  localStorage.removeItem('conversation');
+  }
