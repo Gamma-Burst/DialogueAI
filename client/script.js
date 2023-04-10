fe4592e21271bbdcf8f3c32714f447d326efebfc
Gamma-Burst <christophe.v.mercier@gmail.com>
2023-04-10 23:51:33 +0200

    update the server url

--- client/script.js
+++ client/script.js
@@ -1,122 +1,85 @@
-import bot from './assets/bot.svg'
-import user from './assets/user.svg'
+const form = document.querySelector('form');
+const input = document.querySelector('textarea');
+const chatContainer = document.querySelector('#chat_container');
 
-const form = document.querySelector('form')
-const chatContainer = document.querySelector('#chat_container')
+// Define function to add user input and bot response to chat container
+function addToChatContainer(userInput, botResponse) {
+  const userBubble = document.createElement('div');
+  userBubble.classList.add('user-bubble');
+  userBubble.textContent = userInput;
 
-let loadInterval
+  const botBubble = document.createElement('div');
+  botBubble.classList.add('bot-bubble');
+  botBubble.textContent = botResponse;
 
-function loader(element) {
-    element.textContent = ''
-
-    loadInterval = setInterval(() => {
-        // Update the text content of the loading indicator
-        element.textContent += '.';
-
-        // If the loading indicator has reached three dots, reset it
-        if (element.textContent === '....') {
-            element.textContent = '';
-        }
-    }, 300);
+  chatContainer.appendChild(userBubble);
+  chatContainer.appendChild(botBubble);
+  chatContainer.scrollTop = chatContainer.scrollHeight;
 }
 
-function typeText(element, text) {
-    let index = 0
+// Define function to send user input to server and receive bot response
+async function sendInputToServer(input) {
+  const response = await fetch('/api/chat', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ input: input })
+  });
 
-    let interval = setInterval(() => {
-        if (index < text.length) {
-            element.innerHTML += text.charAt(index)
-            index++
-        } else {
-            clearInterval(interval)
-        }
-    }, 20)
+  const data = await response.json();
+  return data.response;
 }
 
-// generate unique ID for each message div of bot
-// necessary for typing text effect for that specific reply
-// without unique ID, typing text will work on every element
-function generateUniqueId() {
-    const timestamp = Date.now();
-    const randomNumber = Math.random();
-    const hexadecimalString = randomNumber.toString(16);
+// Define function to display chat history in chat container
+async function displayChatHistory() {
+  const response = await fetch('/api/chat-history');
+  const data = await response.json();
+  const questions = data.chatHistory.questions;
+  const responses = data.chatHistory.responses;
 
-    return `id-${timestamp}-${hexadecimalString}`;
+  for (let i = 0; i < questions.length; i++) {
+    addToChatContainer(questions[i], responses[i]);
+  }
 }
 
-function chatStripe(isAi, value, uniqueId) {
-    return (
-        `
-        <div class="wrapper ${isAi && 'ai'}">
-            <div class="chat">
-                <div class="profile">
-                    <img 
-                      src=${isAi ? bot : user} 
-                      alt="${isAi ? 'bot' : 'user'}" 
-                    />
-                </div>
-                <div class="message" id=${uniqueId}>${value}</div>
-            </div>
-        </div>
-    `
-    )
+// Display chat history on page load
+displayChatHistory();
+
+// Function to generate bot response
+async function generateResponse(input) {
+  // Check if user input is empty or already in chat history
+  if (!input || isRepeated(input)) {
+    return 'I cannot understand or repeat the same question.';
+  }
+
+  // Send user input to OpenAI API to get response
+  const response = await openaiChat.getResponse(input);
+
+  // Add user input and bot response to chat history
+  chatHistory.push({ user: input, bot: response });
+
+  // Return the bot response
+  return response;
 }
 
-const handleSubmit = async (e) => {
-    e.preventDefault()
-
-    const data = new FormData(form)
-
-    // user's chatstripe
-    chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
-
-    // to clear the textarea input 
-    form.reset()
-
-    // bot's chatstripe
-    const uniqueId = generateUniqueId()
-    chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
-
-    // to focus scroll to the bottom 
-    chatContainer.scrollTop = chatContainer.scrollHeight;
-
-    // specific message div 
-    const messageDiv = document.getElementById(uniqueId)
-
-    // messageDiv.innerHTML = "..."
-    loader(messageDiv)
-
-    const response = await fetch('https://dialogueai.onrender.com', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            prompt: data.get('prompt')
-        })
-    })
-
-    clearInterval(loadInterval)
-    messageDiv.innerHTML = " "
-
-    if (response.ok) {
-        const data = await response.json();
-        const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
-
-        typeText(messageDiv, parsedData)
-    } else {
-        const err = await response.text()
-
-        messageDiv.innerHTML = "Something went wrong"
-        alert(err)
-    }
+// Function to check if user input is already in chat history
+function isRepeated(input) {
+  return chatHistory.some((entry) => entry.user === input);
 }
 
-form.addEventListener('submit', handleSubmit)
-form.addEventListener('keyup', (e) => {
-    if (e.keyCode === 13) {
-        handleSubmit(e)
-    }
-})
+// Route to handle user input and return bot response
+app.post('/api/chat', async (req, res) => {
+  // Get user input from request body
+  const userInput = req.body.input;
 
+  // Generate response from OpenAI API
+  const botResponse = await generateResponse(userInput);
+
+  // Return bot response in JSON format
+  res.json({ response: botResponse });
+});
+
+// Route to get chat history
+app.get('/api/chat-history', (req, res) => {
+  res.json({ chatHistory });
+});
 
--- server/server.js
+++ server/server.js
@@ -11,30 +11,49 @@ app.use(express.json());
 
 const openaiChat = new OpenAIChat(process.env.OPENAI_API_KEY);
 
-const chatHistory = [];
+// Define chat history object to store all questions and responses
+const chatHistory = {
+  questions: [],
+  responses: []
+};
 
-// Function to generate bot response
-async function generateResponse(input) {
-  // Send user input to OpenAI API to get response
-  const response = await openaiChat.getResponse(input);
-
-  // Add user input and bot response to chat history
-  chatHistory.push({ user: input, bot: response });
-
-  // Return the bot response
-  return response;
+// Define function to add question and response to chat history object
+function addToChatHistory(question, response) {
+  chatHistory.questions.push(question);
+  chatHistory.responses.push(response);
 }
 
-// Route to handle chat requests
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'Hello from CodeX!'
+  });
+});
+
 app.post('/api/chat', async (req, res) => {
-  // Get user input from request body
-  const userInput = req.body.input;
+  const input = req.body.input;
 
-  // Generate response from OpenAI API
-  const botResponse = await generateResponse(userInput);
+  // If chat history is not empty, check if the current input matches a previous question
+  if (chatHistory.questions.length > 0) {
+    const prevIndex = chatHistory.questions.indexOf(input.toLowerCase());
+    if (prevIndex !== -1) {
+      const prevResponse = chatHistory.responses[prevIndex];
+      res.json({ response: prevResponse });
+      return;
+    }
+  }
 
-  // Return bot response in JSON format
-  res.json({ response: botResponse });
+  // If input is not a previous question, generate response using OpenAI API
+  const response = await openaiChat.getResponse(input);
+
+  // Add question and response to chat history
+  addToChatHistory(input.toLowerCase(), response);
+
+  // Return response in JSON format
+  res.json({ response: response });
+});
+
+app.get('/api/chat-history', (req, res) => {
+  res.json({ chatHistory: chatHistory });
 });
 
 app.listen(5000, () => console.log('AI server started on http://localhost:5000'));
