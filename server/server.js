92255a53003a92e36b5b6b3e40c2c00f90adbfbb
Gamma-Burst <christophe.v.mercier@gmail.com>
2023-04-10 23:12:52 +0200

    update the server url

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-import express from 'express'
-import * as dotenv from 'dotenv'
-import cors from 'cors'
-import { Configuration, OpenAIApi } from 'openai'
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from 'openai';
 
-dotenv.config()
+dotenv.config();
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -11,17 +11,17 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 let memory = [];
 
 app.get('/', async (req, res) => {
   res.status(200).send({
     message: 'Hello from CodeX!'
-  })
-})
+  });
+});
 
 app.post('/', async (req, res) => {
   try {
@@ -50,9 +50,17 @@ app.post('/', async (req, res) => {
     });
 
   } catch (error) {
-    console.error(error)
+    console.error(error);
     res.status(500).send(error || 'Something went wrong');
   }
-})
+});
 
-app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
+// Endpoint to clear memory
+app.delete('/', async (req, res) => {
+  memory = [];
+  res.status(200).send({
+    message: 'Memory cleared'
+  });
+});
+
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'));
