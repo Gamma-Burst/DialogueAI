c915d606a3d851e5e6bc60b2e7b25db3e9ed5971
Gamma-Burst <christophe.v.mercier@gmail.com>
2023-04-10 22:30:07 +0200

    update the server url

--- server/server.js
+++ server/server.js
@@ -15,6 +15,9 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+// Create an object to store the chatbot's memory
+const memory = {};
+
 app.get('/', async (req, res) => {
   res.status(200).send({
     message: 'Hello from CodeX!'
@@ -25,9 +28,12 @@ app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
 
+    // Add the prompt to the memory object
+    memory[prompt] = memory[prompt] || [];
+
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `${prompt}`,
+      prompt: `${prompt}${memory[prompt].length > 0 ? memory[prompt][memory[prompt].length - 1] : ''}`,
       temperature: 0, // Higher values means the model will take more risks.
       max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
@@ -35,8 +41,13 @@ app.post('/', async (req, res) => {
       presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
     });
 
+    const botResponse = response.data.choices[0].text;
+
+    // Add the bot response to the memory object
+    memory[prompt].push(botResponse);
+
     res.status(200).send({
-      bot: response.data.choices[0].text
+      bot: botResponse
     });
 
   } catch (error) {
