00381ce2333ddd60dfd62ee196780337d9a41ba8
Gamma-Burst <christophe.v.mercier@gmail.com>
2023-04-10 20:22:37 +0200

    update the server url

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://codex-1z8x.onrender.com', {
+    const response = await fetch('https://dialogueai.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
